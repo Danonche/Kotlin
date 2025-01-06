4dd5c6ba6dfcaed8d53efa16e04e490894bac47c
Daniil Larionov <127932428+Danonche@users.noreply.github.com>
2025-01-06 13:57:08 +0300

    Kotlin: variables and println

---
+++ src/Main.kt
@@ -0,0 +1,23 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    var myAge: Int = 32 // примерно 25 млрд
+    var myShort: Short = 25 // короче в 2 раза чем инт
+    var myLong: Long = 57 // Длиннее чем инт в 2 раза
+
+
+    var myName: String = "Daniil"
+    var myChar: Char = '?' // хранит один символ    var myFloat: Float = 2.44444f
+    var myDouble: Double =  2.444449894894849
+    var myBoolean: Boolean = true
+
+
+    val myVel: Int = 1000   // val нельзя перезаписывать, тоесть после объявления такой переменной нельзя сделать myVal = 2000
+
+    var myNull: Int?= null
+
+
+    println(myAge)
+    println(myName)
+    println(myChar)
+}
